24b9e30ffca75dd467b44986b213d4ff973d25dd
mohammadezzat93 <mohammadd9390@gmail.com>
2021-09-05 19:33:15 +0300

    Add Map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            lat: '',
+            lon: '',
+            zoom : '',
+            displayName: '',
+            mapFlag: false,
+            err: false
+        }
+    }
+
+    getData = async e => {
+        e.preventDefault();
+
+        let cityName = e.target.cityName.value;
+
+        let Key = 'pk.024f6e8bc1742f77c30108e11db494a8';
+        let URL = `https://eu1.locationiq.com/v1/search.php?key=${Key}&q=${cityName}&format=json`;
+        //  console.log('Show', show);
+
+
+        // 2 : axios
+        try {
+            let result = await axios.get(URL);
+            console.log(result);
+
+            this.setState({
+                displayName: result.data[0].display_name,
+                lat: result.data[0].lat,
+                lon: result.data[0].lon,
+                zoom : result.data[0].boundingbox,
+                mapFlag: true
+
+            });
+        }
+        catch {
+            this.setState({
+                err: true
+            })
+        }
+
+    }
+
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.getData}>
+                    <input type="text" name="cityName" />
+                    <input type="submit" value="Show" />
+                </form>
+
+                <h3>{this.state.displayName}</h3>
+                <h3>{this.state.lat}</h3>
+                <h3>{this.state.lon}</h3>
+                <h3>{this.state.boundingbox}</h3>
+
+                {this.state.mapFlag &&
+                    <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.024f6e8bc1742f77c30108e11db494a8&center=${this.state.lat},${this.state.lon} &zoom=${this.state.boundingbox} `} alt='map' />
+                }
+                {this.state.err && <p>Sorry, Not Found</p>}
+            </>
+        );
+    }
 }
 
 export default App;
